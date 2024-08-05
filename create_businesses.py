cf79fffa2bb684d3892ab7619b4a487b11c7b9c5
Caleb Olusola <calebseyi123@gmail.com>
2024-08-05 20:50:02 +0100

    fix

--- create_businesses.py
+++ create_businesses.py
@@ -380,7 +380,7 @@ unique_names = {
     "Content Creators": ["MythBusters", "Veritasium", "VSauce"],
 }
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://hopterlink.up.railway.app"
 BUSINESSES_PER_SUBCATEGORY = 3
 
 
